aae1c7821f72da9f1d791b14ad810e9be591552e
Farjana Eti <etimourakkhieti@gmail.com>
2025-07-03 23:54:44 +0600

    contract page ok

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,115 @@
-import React from 'react'
 
-export default function ContactPage() {
+"use client";
+import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const ContactPage = () => {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  console.log(error);
+  const text = "Say Hello";
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setError(false);
+    setSuccess(false);
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        form.current,
+        process.env.NEXT_PUBLIC_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setSuccess(true);
+          form.current.reset();
+        },
+        () => {
+          setError(true);
+        }
+      );
+  };
+
   return (
-    <div>ContactPage</div>
-  )
-}
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex justify-center items-center">
+  <div className="">
+    {text.split("").map((letter, index) => (
+      <motion.span
+        key={index}
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 0 }}
+        transition={{
+          duration: 3,
+          repeat: Infinity,
+          delay: index * 0.1,
+        }}
+        className="text-6xl mb-8 inline-block"
+      >
+        {letter}
+      </motion.span>
+    ))}
+
+    <p className="font-semibold my-6">
+      Feel free to reach out to me through any of the methods below.
+    </p>
+    <h2 className="font-semibold mb-6">
+      Get in touch or shoot me an email directly by filling the right side form
+    </h2>
+    <h1 className="text-2xl mb-6 font-semibold">or</h1>
+    <h2 className="font-semibold">Contact me on: 017******945</h2>
+  </div>
+</div>
+
+        {/* FORM CONTAINER */}
+        <form
+          onSubmit={sendEmail}
+          ref={form}
+          className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
+        >
+          <span>Dear Farjana Eti</span>
+          <textarea
+            rows={6}
+            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+            name="user_message"
+          />
+          <span>Your mail address is:</span>
+          <input
+            name="user_email"
+            type="text"
+            className="bg-transparent border-b-2 border-b-black outline-none"
+          />
+          <span>Regards</span>
+          <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">
+            Send
+          </button>
+          {success && (
+            <span className="text-green-600 font-semibold">
+              Your message has been sent successfully!
+            </span>
+          )}
+          {error && (
+            <span className="text-red-600 font-semibold">
+              Something went wrong!
+            </span>
+          )}
+        </form>
+      </div>
+    </motion.div>
+  );
+};
+
+export default ContactPage;
+//service---service_i3cgkqq
